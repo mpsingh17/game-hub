24f1bd3b875f9a244e590caf19e89752e2b7b6c4
Manpreet <manpreet@coolpak.co.nz>
2023-12-08 15:25:40 +1300

    code cleanup.

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
     params: {
-        key: "78545154d8b945ddba23815225ab4dc7"
+        key: import.meta.env.VITE_REACT_APP_API_KEY
     }
 });
