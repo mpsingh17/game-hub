6fd7825d9a699f7ddc70ac32e717c643d4b55f7a
Manpreet <manpreet@coolpak.co.nz>
2023-12-10 11:31:08 +1300

    Display platform icons.

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,8 @@
 import { Game } from "../hooks/useGames";
 import { Card, CardBody } from "@chakra-ui/card";
 import { Image } from "@chakra-ui/image";
-import { Heading } from "@chakra-ui/layout";
+import { Heading, Text } from "@chakra-ui/layout";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
   game: Game;
@@ -13,6 +14,9 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformIconList
+          platforms={game.parent_platforms.map(({ platform }) => platform)}
+        />
       </CardBody>
     </Card>
   );
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -10,7 +10,7 @@ const GameGrid = () => {
       {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        padding={5}
+        paddingX={2}
         spacing={10}
       >
         {games.map((game) => (
