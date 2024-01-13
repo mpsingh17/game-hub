245f7c4b1fe05a643659e350f3b9c56a826a194f
Manpreet <mpsingh17@outlook.co.nz>
2024-01-13 22:46:46 +1300

    refactoring Platform interface and remove it from the useGames hook.

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import parentPlatforms from "../data/parentPlatforms";
 import apiClient, { FetchResponse } from "../services/api-client";
 
-interface Platform {
+export interface Platform {
   id: number;
   name: string;
   slug: string;
