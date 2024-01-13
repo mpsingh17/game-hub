8f17dcdd8d13ec09dbfd6b2850f028a2ec39fb0e
Manpreet Singh <manpreet@coolpak.co.nz>
2024-01-13 13:12:23 +1300

    Add React Query and React Query Devtools. Refactor Genres hooks to fetch genres using React Query.

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 import theme from "./theme";
 import "./index.css";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <ChakraProvider theme={theme}>
     <React.StrictMode>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </React.StrictMode>
   </ChakraProvider>
 );
